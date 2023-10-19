b511f07157d4abfc8a99e3d79402c3d8a8857ff6
Davis Keeney <142953893+wdavisk123@users.noreply.github.com>
2023-10-19 22:33:28 +0000

    change

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Davis from the console");
 };
